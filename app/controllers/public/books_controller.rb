7f77e13c17c3cefd34074917e217572dc2953420
lejour <29193321+lejour@users.noreply.github.com>
2023-10-05 07:24:45 +0000

    post

--- app/controllers/public/books_controller.rb
+++ app/controllers/public/books_controller.rb
@@ -4,7 +4,6 @@ class Public::BooksController < ApplicationController
     if params[:keyword]
       @books = RakutenWebService::Books::Book.search(title: params[:keyword])
     end
-    @book = RakutenWebService::Books::Book.search(isbn:"9784297114626")
   end
 
   def show
